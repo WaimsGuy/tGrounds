e062d79ca3d81b14cc4ee8081e1133a1b16f114e
Jordan <riosjordan25@gmail.com>
2023-03-07 15:46:15 -0600

    THE BIG UPDATE (COMMENTED CODE EDITION) Made updates to the shop feature including working buy function that updates shopkeep.txt after each purchase. The long awaited first pass at code commenting has arrived! Please let me know how I can comment better.

--- trainingGrounds.py
+++ trainingGrounds.py
@@ -2,15 +2,14 @@ helplist = {'Help': 'Displays list of actions', 'Calculator': 'Opens calculator!
 cataloglist = dict()
 
 
-# create global variable currency
+# create initial global variable currency
 def initgold():
 	currency = 500
 	return currency
 
 
-# grab global currency variable
+# grab initial currency variable
 def grabgold():
-	global gold
 	gold = initgold()
 	return gold
 
@@ -18,10 +17,10 @@ def grabgold():
 # reset shopkeep.txt to original state
 def resetshop():
 	fhand = open('shopkeep.txt', 'w')
-	fhand.write('Sword,3,120\nDagger,2,60\nPotion,0,25')
+	fhand.write('Sword,3,120\nDagger,2,60\nPotion,0,25')  # note: change this code not shopkeep.txt
 
 
-# split each shopkeep.txt line into a list then nest each list into shopstock
+# split each shopkeep.txt line into a list then nest each list into another list shopstock
 def itemsplit():
 	fhand = open('shopkeep.txt', 'r')
 	grabshop = fhand.read().splitlines()
@@ -42,23 +41,31 @@ def displayitems():
 
 
 # checks if user specified item is in shopstock created by itemsplit func
+# note: quantitycheck func does same thing, redundant func?
 def itemcheck(item):
 	shopstock = itemsplit()
 	for i in range(len(shopstock)):
 		if item != shopstock[i][0]:
 			continue
 		elif item == shopstock[i][0]:
-			return 'true'
+			if int(shopstock[i][1]) > 0:
+				return 'true'
+			else:
+				return 'false'
 
 
 # function to check if user specified quantity is less than or equal to shopstock qty
+# fucntion also calculates total price and finds position of specified item
+# note: does this func do too many things?
 def quantitycheck(item, quantity):
 	shopstock = itemsplit()
 	for i in range(len(shopstock)):
-		if item == shopstock[i][0] and int(quantity) <= int(shopstock[i][1]):
+		if not (item == shopstock[i][0]):
+			continue
+		elif item == shopstock[i][0] and int(quantity) <= int(shopstock[i][1]):
 			hasqty = 'true'
-			price = int(shopstock[i][2]) * int(shopstock[i][1])
-			position = shopstock[i].index(item)
+			price = int(shopstock[shopstock.index(shopstock[i])][2]) * int(quantity)  # finds user specified item price
+			position = shopstock.index(shopstock[i])  # finds position of specified item, used for updatestock func
 			return [hasqty, price, position]
 		else:
 			hasqty = 'false'
@@ -69,40 +76,41 @@ def quantitycheck(item, quantity):
 def updatestock(item, quantity, position):
 	shopstock = itemsplit()
 	if quantitycheck(item, quantity)[0] == 'true':
-		print(shopstock)
-		print(shopstock[position][1])
 		newqty = int(shopstock[position][1]) - int(quantity)
-		print(newqty)
 		shopstock[position][1] = str(newqty)
-		print(shopstock[0][0] + ',' + shopstock[0][1] + ',' + shopstock[0][2])
 		fhand = open('shopkeep.txt', 'w')
 		for i in range(len(shopstock)):
 			fhand.write(shopstock[i][0] + ',' + shopstock[i][1] + ',' + shopstock[i][2] + '\n')
 
 
+# main function interface for buying items
 def sbuy(currentgold):
 	print(currentgold)
-	displayitems()
+	displayitems()  # display available items that are in stock
 	buyitem = input('What would you like to purchase?\n')
-	if itemcheck(buyitem) == 'true':
+	if itemcheck(buyitem) == 'true':  # check if user specified item exists with qty > 0
 		buyqty = input('How many would you like to buy?\n')
-		if quantitycheck(buyitem, buyqty)[0] == 'true':
-			price = quantitycheck(buyitem, buyqty)[1]
-			position = quantitycheck(buyitem, buyqty)[2]
-			if currentgold >= price:
+		if quantitycheck(buyitem, buyqty)[0] == 'true':  # check if user specified qty exceeds available stock
+			price = quantitycheck(buyitem, buyqty)[1]  # grab total cost value from quantitycheck func
+			position = quantitycheck(buyitem, buyqty)[2]  # grab position of specified item within parent list
+			if currentgold >= price:  # check if player has enough gold
 				print("You have enough gold! Enjoy!")
-				currentgold -= price
+				currentgold -= price  # update player gold after purchase
 				updatestock(buyitem, buyqty, position)
-				shop(currentgold)
+				shop(currentgold)  # return to shop function with new gold value
 			else:
 				print("Sorry Link, I can't GIVE credit. Come back when you're a little mmmm RICHER!")
-		else:
+		elif quantitycheck(buyitem, buyqty)[0] == 'false':
 			print("I don't have that many of that item!")
+	elif itemcheck(buyitem) == 'false':  # when user tries to force buy item with qty 0
+		print('That item is out of stock!')
 	else:
 		print("I don't have that item.")
 	shop(currentgold)
 
 
+# main shop directory
+# note: need to implement sell and inventory func
 def shop(currency):
 	currentgold = currency
 	print('Welcome adventurer! Have you come to browse my wares?')
@@ -112,31 +120,36 @@ def shop(currency):
 		sbuy(currentgold)
 
 
+# does this need a comment?
 def getfile():
 	file = input('What file would you like to access? ') + '.txt'
 	return file
 
 
+# grab file and print contents
 def readfile():
 	file = getfile()
 	fhand = open(file, 'r')
 	print(fhand.read())
 
 
+# take user input and append to file specified by getfile func
 def appendfile():
 	file = getfile()
 	fhand = open(file, 'a')
 	newcontent = input('What text would you like to add to ' + file + '?')
-	fhand.write('\n' + newcontent)
+	fhand.write(newcontent + '\n')
 	print('Content, "' + newcontent + '", has been added to ' + file + '!')
 
 
+# does this need a comment?
 def clearfile():
 	file = getfile()
 	fhand = open(file, 'w')
 	fhand.write("")
 
 
+# main file access feature directory
 def accessfile():
 	ftask = input('Would you like to read, write to, or clear a file? ')
 	if ftask == 'read':
@@ -155,12 +168,13 @@ def accessfile():
 		accessfile()
 
 
+# calculator feature
+# note: should I add a main directory for this?
 def compute():
 	print('Welcome to Calculator!')
 	number1 = input('Please type the first number: ')
 	number2 = input('Please type the second number: ')
-	mathsign = input('Would you like to add, subtract, multiply, or divide? ')
-
+	mathsign = input('Would you like to add, subtract, multiply, or divide? ')  # case sensitive
 	if mathsign == 'add':
 		answer = (int(number1) + int(number2))
 	elif mathsign == 'subtract':
@@ -172,17 +186,12 @@ def compute():
 			answer = 'Cannot divide by zero!'
 		else:
 			answer = (int(number1) / int(number2))
-	elif mathsign == 'titan':
-		answer = 'Dispensing crayons...'
-	elif mathsign == 'warlock':
-		answer = 'Go up...'
-	elif mathsign == 'hunter':
-		answer = 'Shatterskate go brrr...'
 	else:
 		answer = 'I cannot do that.'
 	print(answer)
 
 
+# early function, grabs cataloglist (which is actually a dictionary) from top of file, could be reworked
 def catalog():
 	global cataloglist
 	ctask = input('Would you like to view, append to, delete item from, or clear the catalog? ')
@@ -217,26 +226,27 @@ def catalog():
 	catalog()
 
 
+# main program directory to access various features.
 def directory():
 	task = input('What would you like to do? ')
 	if task == 'help':
-		print(helplist)
+		print(helplist)  # could be a function, grabs dictionary helplist
 	elif task == 'calculator':
 		compute()
 	elif task == 'catalog':
-		print('Welcome to the catalog!')
+		print('Welcome to the catalog!')  # prints before calling func to prevent repeated printing within func
 		catalog()
 	elif task == 'file':
-		print('Welcome to file access!')
+		print('Welcome to file access!')  # prints before calling func to prevent repeated printing within func
 		accessfile()
-	elif task == 'shop':
+	elif task == 'shop':  # resets changes made if shop feature was ran previously before running
 		initgold()
 		resetshop()
 		gold = grabgold()
-		shop(gold)
+		shop(gold)  # could I just do shop(grabgold())?
 	else:
 		print('I am not sure what you are asking. For a list of actions type help in the prompt.')
 	directory()
 	
 
-directory()
+directory()  # the first and only func this program calls automatically upon startup
